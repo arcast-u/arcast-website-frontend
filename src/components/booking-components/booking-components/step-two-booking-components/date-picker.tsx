efdbe9b746494f56584a8a574a15bdb3fb9dacdd
BintaSani <bintasani992@gmail.com>
2025-02-10 07:48:32 +0100

    date picker fix for vaercel

--- src/components/booking-components/booking-components/step-two-booking-components/date-picker.tsx
+++ src/components/booking-components/booking-components/step-two-booking-components/date-picker.tsx
@@ -1,69 +1,69 @@
-// 'use client'
-// import React, {useState} from "react";
-// import { Calendar } from "@/components/ui/calendar";
-// import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-// import { Button } from "@/components/ui/button";
-// import { format } from "date-fns";
-// import { TbCaretDown, TbCaretUp } from "react-icons/tb";
-// import Image from "next/image";
+'use client'
+import React, {useState} from "react";
+import { Calendar } from "@/components/ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import { TbCaretDown, TbCaretUp } from "react-icons/tb";
+import Image from "next/image";
 
-// const DatePicker = () => {
-//   const [date, setDate] = useState<Date | undefined>(new Date(2025, 0, 25)); 
-//     const [tempDate, setTempDate] = useState<Date | undefined>(date);
-//   const [isOpen, setIsOpen] = useState(false);
+const DatePicker = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date(2025, 0, 25)); 
+    const [tempDate, setTempDate] = useState<Date | undefined>(date);
+  const [isOpen, setIsOpen] = useState(false);
 
-//   // Unavailable dates
-//   const disabledDates = [
-//     new Date(2025, 1, 4),
-//     new Date(2025, 1, 14),
-//     new Date(2025, 1, 24),
-//     new Date(2025, 1, 19),
-//   ];
+  // Unavailable dates
+  const disabledDates = [
+    new Date(2025, 1, 4),
+    new Date(2025, 1, 14),
+    new Date(2025, 1, 24),
+    new Date(2025, 1, 19),
+  ];
 
-//   const handleDone = () => {
-//     setDate(tempDate);
-//     setIsOpen(false);
-//   };
+  const handleDone = () => {
+    setDate(tempDate);
+    setIsOpen(false);
+  };
 
 
-//   return (
-//     <div className="flex flex-col items-start gap-2">
-//       <Popover open={isOpen} onOpenChange={setIsOpen}>
-//         <PopoverTrigger asChild>
-//           <Button className={`flex items-center justify-between hover:bg-[#F5F5F7] mt-4 3xl:mt-5 w-full rounded-xl bg-[#F5F5F7] ${isOpen ? "border-[0.5px] border-[#5081FF]" : ""}`}>
-//             <div className="flex items-center justify-between w-full ">
-//               <div className="flex gap-4 items-center">
-//                 <Image src="/icons/calendar.svg" width={44} height={44} alt="Calendar" className="object-contain 3xl:size-[54px]" />
-//                 <div className="flex flex-col font-nunitoSans">
-//                   <p className="3xl:text-base text-sm text-left font-normal text-[#989898]">Select a day</p>
-//                   <p className="mt-1 text-base leading-5 3xl:text-xl 3xl:leading-[27.28px] text-left font-semibold text-[#333333]">
-//                     {date ? format(date, "dd-MM-yyyy") : "Select a date"}
-//                   </p>
-//                 </div>
-//               </div>
-//               {isOpen ?  <TbCaretDown className="size-5 text-[#333333] stroke-[1px]" /> : <TbCaretUp className="size-5 text-[#333333] stroke-[1px]" />}
-//             </div>
-//           </Button>
-//         </PopoverTrigger>
-//         <PopoverContent className="w-[100%] shadow-xl bg-[#F5F5F7] z-20 p-2">
-//           <Calendar
-//             mode="single"
-//             selected={tempDate}
-//             onSelect={setTempDate}
-//             disabled={(date) => disabledDates.some((d) => d.toDateString() === date.toDateString())}
-//           />
-//           <div className="mt-1 mb-3 mr-3 flex justify-end">
-//             <Button  onClick={handleDone} className="bg-[#FF8C42] hover:bg-[#FF8C42] w-fit font-nunitoSans font-medium text-sm leadeing-[19px] whitespace-nowrap shadow-[0px_4px_4px_rgba(0,10,31,0.2)] border-[#FFC49D] text-[#FCFCFC] py-3 px-5 rounded-lg hover:scale-105">
-//               Done
-//             </Button>
-//           </div>
-//         </PopoverContent>
-//       </Popover>
-//     </div>
-//   );
-// };
+  return (
+    <div className="flex flex-col items-start gap-2">
+      <Popover open={isOpen} onOpenChange={setIsOpen}>
+        <PopoverTrigger asChild>
+          <Button className={`flex items-center justify-between hover:bg-[#F5F5F7] mt-4 3xl:mt-5 w-full rounded-xl bg-[#F5F5F7] ${isOpen ? "border-[0.5px] border-[#5081FF]" : ""}`}>
+            <div className="flex items-center justify-between w-full ">
+              <div className="flex gap-4 items-center">
+                <Image src="/icons/calendar.svg" width={44} height={44} alt="Calendar" className="object-contain 3xl:size-[54px]" />
+                <div className="flex flex-col font-nunitoSans">
+                  <p className="3xl:text-base text-sm text-left font-normal text-[#989898]">Select a day</p>
+                  <p className="mt-1 text-base leading-5 3xl:text-xl 3xl:leading-[27.28px] text-left font-semibold text-[#333333]">
+                    {date ? format(date, "dd-MM-yyyy") : "Select a date"}
+                  </p>
+                </div>
+              </div>
+              {isOpen ?  <TbCaretDown className="size-5 text-[#333333] stroke-[1px]" /> : <TbCaretUp className="size-5 text-[#333333] stroke-[1px]" />}
+            </div>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[100%] shadow-xl bg-[#F5F5F7] z-20 p-2">
+          <Calendar
+            mode="single"
+            selected={tempDate}
+            onSelect={setTempDate}
+            disabled={(date) => disabledDates.some((d) => d.toDateString() === date.toDateString())}
+          />
+          <div className="mt-1 mb-3 mr-3 flex justify-end">
+            <Button  onClick={handleDone} className="bg-[#FF8C42] hover:bg-[#FF8C42] w-fit font-nunitoSans font-medium text-sm leadeing-[19px] whitespace-nowrap shadow-[0px_4px_4px_rgba(0,10,31,0.2)] border-[#FFC49D] text-[#FCFCFC] py-3 px-5 rounded-lg hover:scale-105">
+              Done
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
 
-// export default DatePicker;
+export default DatePicker;
 
 // 'use client'
 // import React, {useState} from "react";
