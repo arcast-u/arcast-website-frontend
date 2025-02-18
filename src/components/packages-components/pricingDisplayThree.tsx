1fb891a7594d13ada4d54ef86b09a8daff74f9fe
BintaSani <bintasani992@gmail.com>
2025-02-18 18:47:50 +0100

    bundles mobile

--- src/components/packages-components/pricingDisplayThree.tsx
+++ src/components/packages-components/pricingDisplayThree.tsx
@@ -35,7 +35,38 @@ const pricingItems: PricingItemProps[] = [
 export const PricingDisplayUltimate = () => {
     return (
       <div className="flex justify-center max-w-[1400px]  mx-auto">
-        <div className="grid grid-cols-[1fr,auto,1fr] gap-10 items-start">
+        <div className='md:hidden grid grid-cols-[auto,1fr] gap-6 items-start justify-start'>
+            {pricingItems.map((item, index) => (
+                <React.Fragment key={index}>
+    
+                {/* Timeline - only render once */}
+                {index === 0 && (
+                    <div className="row-span-9">
+                        <Timeline  maxSteps={4} />
+                    </div>
+                )}
+    
+                {/* Right side */}
+                <div className={`${index > 0 ? " mt-[70px]" : "mt-0"}`}>
+                    
+                    <div className="flex flex-col mb-4">
+                        <h2 className="3xl:text-2xl text-[#333333] font-hankenGrotesk font-normal text-base leading-5">{item.title}</h2>
+                        <p className="3xl:mt-2 mt-1 text-[#989898] 3xl:text-2xl font-nunitoSans text-lg leading-[19.1px] font-medium">{item.price}</p>
+                    </div>
+                    <Image
+                        loading="lazy"
+                        width={287}
+                        height={260}
+                        src={item.imageSrc}
+                        alt={item.title || "Pricing item image"}
+                        className="object-contain rounded-xl 3xl:w-[453px] 3xl:h-[360px] max-md:max-w-full"
+                    />
+                    
+                </div>
+                </React.Fragment>
+            ))}
+        </div>
+        <div className="hidden md:grid grid-cols-[1fr,auto,1fr] gap-10 items-start">
           {pricingItems.map((item, index) => (
             <React.Fragment key={index}>
               {/* Left side */}
