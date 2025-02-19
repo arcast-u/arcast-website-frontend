dc15b4b3e17d63a04ccd50f88f01d8ece5e1c9a0
BintaSani <bintasani992@gmail.com>
2025-02-19 19:29:21 +0100

    space

--- src/components/homepage-components/professional-studio/professional-studio.tsx
+++ src/components/homepage-components/professional-studio/professional-studio.tsx
@@ -4,7 +4,7 @@ import Carousel3D from './3d-carousel';
 
 function ProfessionalStudio() {
   return (
-    <div className='w-full lg:h-screen 3xl:h-auto  md:pt-0 lg:pt-0 pb-2 3xl:pb-4'>
+    <div className='w-full lg:h-screen 3xl:h-auto  md:pt-0 lg:pt-5 3xl:pt-0 pb-2 3xl:pb-4'>
         <ProfessionalStudioHeader />
         <div className='h-[50%] md:h-[80%] lg:h-[70%]'>
         
