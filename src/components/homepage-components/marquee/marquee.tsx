eb2e336e6d62fd2e1b3425f607ce858a3bddf8a8
BintaSani <bintasani992@gmail.com>
2025-02-20 20:45:59 +0100

    testimonial

--- src/components/homepage-components/marquee/marquee.tsx
+++ src/components/homepage-components/marquee/marquee.tsx
@@ -19,14 +19,14 @@ const MarqueeSlider =() => {
       <div className=' bg-[#0C0C0C] py-3 3xl:py-6 mx-auto '>
         <Marquee gradient={true} gradientColor='#0C0C0C' speed={100}>
           <div className='flex items-center md:gap-10 gap-5 pl-10 '>
-            <Image src={images[0]} alt='y!' width={516} height={27} className='w-auto' />
+            <Image src={images[0]} alt='y!' width={321} height={27} className='w-auto' />
             <Image
               src={images[1]}
               alt='business insider'
               width={13}
               height={27}
             />
-            <Image src={images[2]} alt='the guardian' width={381} height={27} />
+            <Image src={images[2]} alt='the guardian' width={597} height={27} />
             <Image
               src={images[3]}
               alt='business insider'
@@ -40,7 +40,7 @@ const MarqueeSlider =() => {
               width={13}
               height={27}
             />
-            <Image src={images[6]} alt='this Day' width={229} height={27} />
+            <Image src={images[6]} alt='this Day' width={597} height={27} />
             <Image
               src={images[7]}
               alt='business insider'
