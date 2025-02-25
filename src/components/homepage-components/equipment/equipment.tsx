f632d4683dd94a8c1e75a75dc5818cdb4c0e0c20
BintaSani <bintasani992@gmail.com>
2025-02-25 16:36:41 +0100

    changes

--- src/components/homepage-components/equipment/equipment.tsx
+++ src/components/homepage-components/equipment/equipment.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 function Equipment() {
   return (
-    <div className='bg-[#f5f5f7] w-full px-[21px] py-10 3xl:py-20 lg:mt-24 3xl:mt-0 lg:px-10 3xl:px-[100px]'>
-      <div className='max-w-[1728px] h-full mx-auto'>
+    <div className='bg-[#f5f5f7] w-full  py-10 3xl:py-20 lg:mt-24 3xl:mt-0 '>
+      <div className='max-w-[1728px] h-full px-[21px] mx-auto lg:px-10 3xl:px-[100px]'>
         <header className="flex gap-5 justify-between mx-auto mb-[51px] 3xl:mb-[121px] w-full text-[#333333]">
           <div className="flex flex-col font-hankenGrotesk">
             <h1 className="text-[32px] leading-[41.7px] 3xl:text-5xl 3xl:leading-[62.54px] font-medium max-md:text-4xl">
