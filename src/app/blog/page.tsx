b84fd90ff6a2f7e480a559171f8a79ccbb0b83c2
BintaSani <bintasani992@gmail.com>
2025-02-27 07:28:41 +0100

    vercel fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,4 @@
 import BlogPage from '@/components/blog-components/blog-page/blogPage'
-import NavigationBar from '@/components/homepage-components/nav-bar'
 import NewsletterSignup from '@/components/homepage-components/newsletter/newsletter'
 import React from 'react'
 import Footer from '@/components/homepage-components/footer/footer'
