dc9c067f10710595b52b48e9a9fba787d8dd1140
peaceabiodun <abiodunpeace8@gmail.com>
2025-04-23 11:19:53 +0100

    updates

--- src/components/language-translate/page.tsx
+++ src/components/language-translate/page.tsx
@@ -11,12 +11,16 @@ interface Language {
   code: string;
   name: string;
 }
-// interface LanguageSelectorProps {
-//   isOpen: boolean;
-//   isStudioOpen: boolean;
-//   pathname: string;
-// }
-export default function LanguageSelector() {
+interface LanguageSelectorProps {
+  isOpen: boolean;
+  isStudioOpen: boolean;
+  pathname: string;
+}
+export default function LanguageSelector({
+  isOpen,
+  isStudioOpen,
+  pathname,
+}: LanguageSelectorProps) {
   const [languages] = useState<Language[]>([
     { code: 'en', name: 'ENG' },
     { code: 'ru', name: 'RUS' },
@@ -24,6 +28,8 @@ export default function LanguageSelector() {
 
     // Add more languages as needed
   ]);
+
+  console.log(isOpen, isStudioOpen, pathname, 'isOpen');
 
   const [selectedLanguage, setSelectedLanguage] = useState<string>('en');
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
