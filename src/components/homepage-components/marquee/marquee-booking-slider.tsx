a93976142ca836a295a44c88a9bb8bb559fb3a6e
Henry Omofonmwan <omofonmwanhenrynosa@gmail.com>
2025-05-19 17:53:07 +0100

    This is  a new commit for arcast fixes

--- src/components/homepage-components/marquee/marquee-booking-slider.tsx
+++ src/components/homepage-components/marquee/marquee-booking-slider.tsx
@@ -15,61 +15,80 @@ const images = [
 
 const MarqueeBookingSlider = () => {
   return (
-    <section className='w-full bg-[#0C0C0C] '>
-      <div className=' max-w-[1728px]  py-1 3xl:py-2 mx-auto '>
+    <section className='w-full bg-[#0C0C0C]'>
+      <div className='max-w-[1728px] py-[0.5] 3xl:py-2 mx-auto'>
         <Marquee gradient={true} gradientColor='#0C0C0C' speed={180}>
-          <div className='flex items-center md:gap-18 gap-5 pl-18 '>
+          <div className='flex items-center md:gap-18 gap-3 pl-18'>
+            {/* Wide logo 1 */}
             <Image
               src={images[0]}
               alt='y!'
               width={200}
               height={18}
-              className='w-auto h-[18px]'
+              className='w-auto h-[14px] md:h-[18px]' // Smaller on mobile
             />
+            
+            {/* Small divider logo */}
             <Image
               src={images[1]}
               alt='business insider'
               width={13}
               height={18}
+              className='w-auto h-[12px] md:h-[18px]' // Smaller on mobile
             />
+            
+            {/* Wide logo 2 */}
             <Image
               src={images[2]}
               alt='the guardian'
               width={300}
               height={18}
-              className='w-auto h-[18px]'
+              className='w-auto h-[14px] md:h-[18px]' // Smaller on mobile
             />
+            
+            {/* Small divider logo */}
             <Image
               src={images[3]}
               alt='business insider'
               width={13}
               height={18}
+              className='w-auto h-[12px] md:h-[18px]' // Smaller on mobile
             />
+            
+            {/* Wide logo 3 */}
             <Image
               src={images[4]}
               alt='vanguard'
               width={200}
               height={18}
-              className='w-auto h-[18px]'
+              className='w-auto h-[14px] md:h-[18px]' // Smaller on mobile
             />
+            
+            {/* Small divider logo */}
             <Image
               src={images[5]}
               alt='business insider'
               width={13}
               height={18}
+              className='w-auto h-[12px] md:h-[18px]' // Smaller on mobile
             />
+            
+            {/* Wide logo 4 */}
             <Image
               src={images[6]}
               alt='this Day'
               width={300}
               height={18}
-              className='w-auto h-[18px]'
+              className='w-auto h-[14px] md:h-[18px]' // Smaller on mobile
             />
+            
+            {/* Small divider logo */}
             <Image
               src={images[7]}
               alt='business insider'
               width={13}
               height={18}
+              className='w-auto h-[12px] md:h-[18px]' // Smaller on mobile
             />
           </div>
         </Marquee>
